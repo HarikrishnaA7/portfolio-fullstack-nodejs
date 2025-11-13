d8a333e2ebd5c7a00b1fb1473ae47fb9972f1923
harikrishna <47034781+HarikrishnaA7@users.noreply.github.com>
2025-11-13 00:37:55 -0800

    Add Experience and Skills components to App

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Header from './components/Header';
 import About from './components/About';
 import Projects from './components/Projects';
+import Experience from './components/Experience';
+import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
@@ -11,6 +13,8 @@ function App() {
     <div className="App">
       <Header />
       <About />
+          <Experience />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
